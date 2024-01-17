8e7f5749cee03931eaa94616cbc79088b652ccf6
Angga ps <80311292+anggaps@users.noreply.github.com>
2024-01-17 12:59:50 +0700

    cart sheet

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,15 +1,34 @@
 'use client';
 import { PRODUCT_CATEGORIES } from '@/config';
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import NavItem from './NavItem';
+import { useOnClickOutside } from '@/hooks/use-on-click-outside';
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        setActiveIndex(null);
+      }
+    };
+
+    document.addEventListener('keydown', handler);
+
+    return () => {
+      document.removeEventListener('keydown', handler);
+    };
+  }, []);
+
   const isAnyOpen = activeIndex !== null;
 
+  const navRef = useRef<HTMLDivElement | null>(null);
+
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+
   return (
-    <div className='flex gap-4 h-full'>
+    <div className='flex gap-4 h-full' ref={navRef}>
       {PRODUCT_CATEGORIES.map((category, i) => {
         const handleOpen = () => {
           if (activeIndex === i) {
@@ -28,6 +47,9 @@ const NavItems = () => {
             isOpen={isOpen}
             isAnyOpen={isAnyOpen}
             key={category.value}
+            close={function (): void {
+              throw new Error('Function not implemented.');
+            }}
           />
         );
       })}
